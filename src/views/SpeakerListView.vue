3073a96121ffb3559a86348df54d5df1bb68fc02
Antti S. Lankila <alankila@bel.fi>
2025-06-04 20:20:08 +0300

    Add bare bones search function
    - experimenting with debounce. It's clear that some time delay before
      responding is good in this case due to relatively large number of
      elements that are drawn.

--- src/views/SpeakerListView.vue
+++ src/views/SpeakerListView.vue
@@ -1,8 +1,32 @@
 <script setup lang="ts">
 
 import { metadata } from '@/util/spinorama';
+import { ref, watchEffect, type Ref } from 'vue';
 
-function tonalityScore(speaker: typeof metadata[keyof typeof metadata]) {
+/**
+ * Delay execution of a function by configured delay. New calls reset the delay.
+ * 
+ * @param func  
+ * @param delay 
+ */
+function debounce<T>(func: (...args: T[]) => void, delay = 300) {
+  let timer: any;
+  return (...args: T[]) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => func(...args), delay)
+  }
+}
+
+type mdtype = typeof metadata;
+type Speaker = typeof metadata[keyof mdtype]
+
+const search = ref("");
+
+let filteredMetadata: Ref<[string, Speaker][]> = ref([])
+
+function tonalityScore(speaker: Speaker) {
   // @ts-ignore
   const measurement = speaker.measurements[speaker.default_measurement]?.pref_rating
   if (!measurement) {
@@ -11,22 +35,37 @@ function tonalityScore(speaker: typeof metadata[keyof typeof metadata]) {
   return (12.69 - 2.49 * measurement.nbd_on_axis - 2.99 * measurement.nbd_pred_in_room - 4.31 * Math.log10(measurement.lfx_hz) + 2.32 * measurement.sm_pred_in_room).toFixed(1)
 }
 
-function bassExtension(speaker: typeof metadata[keyof typeof metadata]) {
+function bassExtension(speaker: Speaker) {
   // @ts-ignore
   const measurement = speaker.measurements[speaker.default_measurement]?.pref_rating
   return measurement?.lfx_hz
 }
 
-function flatness(speaker: typeof metadata[keyof typeof metadata]) {
+function flatness(speaker: Speaker) {
   // @ts-ignore
   const measurement = speaker.measurements[speaker.default_measurement]?.estimates
   return measurement?.ref_band?.toFixed(1)
 }
 
-function format(speaker: typeof metadata[keyof typeof metadata]) {
+function format(speaker: Speaker) {
   // @ts-ignore
   return speaker.measurements[speaker.default_measurement]?.format;
 }
+
+const filterMetadata = debounce((lcSearch: string) => {
+  filteredMetadata.value = Object.entries(metadata).filter(entry => {
+    let result = true
+    if (result && lcSearch) {
+      result &&= `${entry[1].brand} ${entry[1].model}`.toLowerCase().indexOf(lcSearch) !== -1
+    }
+    return result
+  })
+});
+
+watchEffect(() => {
+  const lcSearch = search.value.toLowerCase()
+  filterMetadata(lcSearch)
+})
 
 </script>
 
@@ -35,11 +74,11 @@ function format(speaker: typeof metadata[keyof typeof metadata]) {
     <h1 class="font-bold self-center py-4 px-8">Unofficial spinorama<br/>(experimental version)</h1>
 
     <div class="grid self-center">
-      xx
+      <input type="text" v-model="search" placeholder="Brand or model..."/>
     </div>
 
     <div class="bg-spinorama-bg-darker col-span-2 grid gap-4 overflow-y-scroll p-4 justify-center border-t auto-fit-cols">
-      <div class="pb-4 rounded-lg shadow grid items-center justify-items-center grid-rows-1 auto-rows-max gap-2 bg-spinorama-bg" v-for="[speakerId, speaker] of Object.entries(metadata)">
+      <div class="pb-4 rounded-lg shadow grid items-center justify-items-center grid-rows-1 auto-rows-max gap-2 bg-spinorama-bg" v-for="[speakerId, speaker] of filteredMetadata">
         <div><img loading="lazy" class="max-w-[400px] max-h-600px" :src="`pictures/${encodeURI(speakerId)}.webp`"/></div>
         <div class="font-bold">
           {{speaker.brand}} {{speaker.model}}
