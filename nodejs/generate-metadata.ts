160bdbbe223c7cd36325dddd64b12f8258fe12f7
Antti S. Lankila <alankila@bel.fi>
2025-06-13 20:13:36 +0300

    Put logging crap to stderr, pristine json to stdout

--- nodejs/generate-metadata.ts
+++ nodejs/generate-metadata.ts
@@ -3,23 +3,23 @@ import { processSpinoramaFile } from "../src/util/loaders"
 import { getScores, type Scores } from "../src/util/scores";
 import { readdirSync, readFileSync } from "fs"
 
+const dir = "public/measurements"
+
 async function listMeasurements(dir) {
     const files = readdirSync(dir, { recursive: true, encoding: "binary" })
-    return files.filter(f => f.endsWith(".zip")).map(f => dir + "/" + f)
+    return files.filter(f => f.endsWith(".zip"))
 }
 
-console.log("Initialized...")
-let files = await listMeasurements("public/measurements")
-console.log("Found", files.length, "zip files to read")
+let files = await listMeasurements(dir)
 
 let count = 0
 let result: { [key: string]: Scores } = {}
 for (let file of files) {
-    const data = readFileSync(file)
+    const data = readFileSync(`${dir}/${file}`)
     try {
         const [horizSpin, vertSpin] = await processSpinoramaFile(new Uint8Array(data))
         if (horizSpin.isBusted || vertSpin.isBusted) {
-            console.log("Busted measurement")
+            console.warn("Busted measurement", file)
         }
 
         const cea2034 = compute_cea2034(horizSpin, vertSpin)
@@ -28,9 +28,8 @@ for (let file of files) {
         count ++;
     }
     catch (error) {
-        console.log("Unable to process", file, error)
+        console.warn("Unable to process", file, error)
     }
 }
 
-console.log("Read", count, "/", files.length)
 console.log(result)
